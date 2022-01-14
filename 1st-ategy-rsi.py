e8d8adc139932bf0785938131eb38cbe0e815b16
ahmedjoubest <ahmedjoubest@gmail.com>
2022-01-14 09:21:35 +0100

    1

---
+++ 1st-ategy-rsi.py
@@ -0,0 +1,8 @@
+
+# Author: Ahmed
+# 14/01/2022
+# Tuto: https://www.youtube.com/watch?v=pB8eJwg7LJU&t=1838s&ab_channel=Algovibes
+# The first goal is to apply this strategy with Binance API (instead of stocks)
+# for second timeframe
+
+print("te")
