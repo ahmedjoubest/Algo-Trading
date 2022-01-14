99c004e9f2dfb5269ed9ea97d6a185f43f00eff1
ahmedjoubest <ahmedjoubest@gmail.com>
2022-01-14 09:50:29 +0100

    1

--- 1st-ategy-rsi.py
+++ 1st-ategy-rsi.py
@@ -5,4 +5,16 @@
 # The first goal is to apply this strategy with Binance API (instead of stocks)
 # for second timeframe
 
-print("te")
+import pandas as pd
+import sqlalchemy
+from binance.client import Client
+from binance import BinanceSocketManager
+
+# --- 1 API (Confidential)
+api_key = 'YqtSwA9CkxTjBlx2f3NUnBTj7YwH8hj4OZq9USMb7YsRfH18UC3JFS39QL3JgxDy'
+api_secret = 'Ru1Drz8zalkBeTRShKKk8YEGsaeRh6YZ0lukwBZpGxClWiIfGBjB5MLoKd4zlgqw'
+client = Client(api_key,api_secret)
+
+# --- 2 read data
+for kline in client.get_historical_klines_generator("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "24 hours ago UTC"):
+    print(kline)
