a0da2b55052919616662f601dea029fba4e21912
ahmedjoubest <ahmedjoubest@gmail.com>
2022-04-28 19:59:26 +0200

    logging added

--- Backtest/Bot_div_5min_V1.py
+++ Backtest/Bot_div_5min_V1.py
@@ -3,7 +3,7 @@
 # 26/04/2022
 # Function that eliminates crossed pics
 
-# --- 0 Import
+# --- Import
 import pandas as pd
 import numpy as np
 from binance.client import Client
@@ -22,15 +22,16 @@ from binance.enums import HistoricalKlinesType
 import time
 from datetime import datetime
 from datetime import timedelta
-
+import logging
 
 
 
 # --- Sourcing functions
-execfile("functions/get_data.py")
-execfile("functions/HA_calculation.py")
-# execfile("functions/RSI_calculation.py") # We are using RSI
-execfile("functions/math_tools.py")
+# execfile("functions/get_data.py")
+# execfile("functions/math_tools.py")
+exec(open("functions/get_data.py").read())
+exec(open("functions/math_tools.py").read())
+
 
 # --- API
 api_key = 'vCvbNDYnP04sL3ZMGdGxY4QuEPEdotvw9JqBoM7cL9sSUol5m86EZwhy3JOI0kon'
@@ -40,58 +41,46 @@ client = Client(api_key,api_secret)
 
 # The argument "Window" is not being of use yet !
 
+# logging system
+# More on login system : https://algotrading101.com/learn/live-algo-trading-on-the-cloud-aws/
+# https://docs.python.org/fr/3/howto/logging.html
+logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s', filename='events.log', filemode='a')
 
+
+symbol = "WAVESUSDT"
+window_div= 7
+tolerance = 0.25
+levier = 1
 def div_5min(symbol = "WAVESUSDT", window_div= 7, tolerance = 0.25, levier = 1):
+
     while(True):
+
+        # 0 --- traceback
+        print("###### Iteration at: "+str(datetime.now())+" ######")
+        logging.info("###### Iteration at: "+str(datetime.now())+" ######")
 
         # 1 --- Get data and transform it to HA
         try:
             df_5mn = getdata_min_ago(symbol, interval = "5m", lookback= str(13*60))
         except Exception as e:
             print(f'Problem in reading data, exception hya : {e}')
-
+            logging.info(f'Problem in reading data, exception hya : {e}')
+        global HAdf_5mn,RSI_stoch_k,RSI_stoch_d,RSI
         HAdf_5mn = HA_transformation(df_5mn)
         RSI_stoch_k = round(pta.stochrsi(HAdf_5mn['Close']).STOCHRSIk_14_14_3_3, 2)  # k = blue # ignore warning
         RSI_stoch_d = round(pta.stochrsi(HAdf_5mn['Close']).STOCHRSId_14_14_3_3, 2)
         RSI = round(pta.rsi(HAdf_5mn.Close, 14), 2)
 
+
         # 2 --- Verify if the stochastic is verified
         OB_or_OS = sum(HAdf_5mn.tail(3).iloc[[0, 1]].Open < HAdf_5mn.tail(3).iloc[[0, 1]].Close)
-        if (OB_or_OS == 2):
-            # last two candles are green
-            if (((RSI_stoch_d.iloc[[-1]] >= 80) & (RSI_stoch_k.iloc[[-1]] >= 80))[0]):
-                print("next steps: short (stochastic is verified)")
-            else:
-                print("skip: step 2 (stochastic not verified)")
-                continue
-        elif (OB_or_OS == 0):
-            # last two candles are red
-            if (((RSI_stoch_d.iloc[[-1]] <= 20) & (RSI_stoch_k.iloc[[-1]] <= 20))[0]):
-                print("next steps: long (stochastic is verified)")
-            else:
-                print("skip: step 2 (stochastic not verified)")
-                continue
-        else:
-            print("skip: last two candles are not in the same color!")
+        if(not verify_stochastic_5min(OB_or_OS, RSI_stoch_d, RSI_stoch_k)):
             continue
 
+
         # 3 --- Verify condition 5 min
-        # 3 - a - position short
-        if (OB_or_OS == 2):
-            # {High(t) - Low(t)} should be lower than {High(t-1) - Low(t-1)}
-            if ((HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[1] <= (HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[0]):
-                print("next steps (Short): Verify div (condition 5 min verified)")
-            else:
-                print("skip (condition 5 min NOT verified)")
-                continue
-        # 3 - b - position Long
-        if (OB_or_OS == 0):
-            # {High(t) - Low(t)} should be lower than {High(t-1) - Low(t-1)}
-            if ((HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[1] <= (HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[0]):
-                print("next steps (Long): Verify div (condition 5 min verified)")
-            else:
-                print("skip (condition 5 min NOT verified)")
-                continue
+        if(not verify_condition_5min(OB_or_OS,HAdf_5mn)):
+            continue
 
         # 4 --- Find divergence
 
@@ -103,58 +92,19 @@ def div_5min(symbol = "WAVESUSDT", window_div= 7, tolerance = 0.25, levier = 1):
         else:
             peaks, _ = find_peaks(-HAdf_5mn.Close, distance=2)
 
-        # 4 - b - Eliminate invalid last peaks (Stochastic verification)
-        if (OB_or_OS == 2):
-            # short
-            potential_pics = []
-            for idx in peaks:
-                # Stochastic must be verified
-                if (((RSI_stoch_k.iloc[[idx]] >= 65))[0]):
-                    # At least 3 candles same color
-                    if (
-                            sum(HAdf_5mn.iloc[[idx - 1, idx, idx + 1]].Close > HAdf_5mn.iloc[
-                                [idx - 1, idx, idx + 1]].Open) == 3 or \
-                            sum(HAdf_5mn.iloc[[idx - 2, idx - 1, idx]].Close > HAdf_5mn.iloc[
-                                [idx - 2, idx - 1, idx]].Open) == 3 or \
-                            sum(HAdf_5mn.iloc[[idx, idx + 1, idx + 2]].Close > HAdf_5mn.iloc[
-                                [idx, idx + 1, idx + 2]].Open) == 3
-                    ):
-                        potential_pics.append(idx)
-                    else:
-                        continue
-                else:
-                    continue
-        else:
-            # long
-            potential_pics = []
-            for idx in peaks:
-                # Stochastic must be verified
-                if (((RSI_stoch_k.iloc[[idx]] <= 35))[0]):
-                    # At least 3 candles
-                    if (
-                            sum(HAdf_5mn.iloc[[idx - 1, idx, idx + 1]].Close < HAdf_5mn.iloc[
-                                [idx - 1, idx, idx + 1]].Open) == 3 or \
-                            sum(HAdf_5mn.iloc[[idx - 2, idx - 1, idx]].Close < HAdf_5mn.iloc[
-                                [idx - 2, idx - 1, idx]].Open) == 3 or \
-                            sum(HAdf_5mn.iloc[[idx, idx + 1, idx + 2]].Close < HAdf_5mn.iloc[
-                                [idx, idx + 1, idx + 2]].Open) == 3
-                    ):
-                        potential_pics.append(idx)
-                    else:
-                        continue
-                else:
-                    continue
+        # 4 - b - Eliminate invalid last peaks (Stochastic + 3 candles verification)
+        potential_pics = eliminate_last_pics_stocha_candles(OB_or_OS,peaks,HAdf_5mn,RSI_stoch_k)
 
         # ISSUE with find_peaks
         if (OB_or_OS == 2):
             # Issue: if last pic is the highest, this stupid function won't take it, we should add it!
-            max_last_pic = np.where(HAdf_5mn.Close == max(HAdf_5mn.iloc[[-3, -2, -1]].Close))[0][0]
+            max_last_pic = np.where(HAdf_5mn.Close == max(HAdf_5mn.iloc[[-3, -2, -1]].Close))[0][-1] # [-1] to take the last maximum
             if(max_last_pic not in peaks):
                 potential_pics = np.append(potential_pics,max_last_pic)
         # long (minima)
         else:
             # Issue: if last pic is the lowest, this stupid function won't take it, we should add it!
-            min_last_pic = np.where(HAdf_5mn.Close == min(HAdf_5mn.iloc[[-3, -2, -1]].Close))[0][0]
+            min_last_pic = np.where(HAdf_5mn.Close == min(HAdf_5mn.iloc[[-3, -2, -1]].Close))[0][-1]
             if (min_last_pic not in peaks):
                 potential_pics = np.append(potential_pics, min_last_pic)
 
@@ -174,52 +124,13 @@ def div_5min(symbol = "WAVESUSDT", window_div= 7, tolerance = 0.25, levier = 1):
                                HAdf_5mn.iloc[[x]].index > (datetime.now() - timedelta(hours=window_div))]
 
         # 4 - e - Eliminate peaks without divergence (must be reviewed)
-        Div = False
-        for peak in uncrossed_peaks_RSI:
-            if (OB_or_OS == 2):
-                # Short
-                # Hidden divergence:
-                if (32 < RSI[peak] < 68):
-                    if (HAdf_5mn.iloc[[peak]].Close[0] > HAdf_5mn.iloc[[currect_pic]].Close[0] and \
-                            RSI[peak] < RSI[currect_pic]):
-                        Div = True
-                        print("Hidden Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
-                        break
-                    else:
-                        continue
-                elif (RSI[peak] >= 68):
-                    # Hidden divergence:
-                    if (HAdf_5mn.iloc[[peak]].Close[0] < HAdf_5mn.iloc[[currect_pic]].Close[0] and \
-                            RSI[peak] > RSI[currect_pic]):
-                        Div = True
-                        print("Regular Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
-                        break
-                    else:
-                        continue
-            else:
-                # Long
-                # Hidden divergence:
-                if (32 < RSI[peak] < 68):
-                    if (HAdf_5mn.iloc[[peak]].Close[0] < HAdf_5mn.iloc[[currect_pic]].Close[0] and \
-                            RSI[peak] > RSI[currect_pic]):
-                        Div = True
-                        print("Hidden Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
-                        break
-                    else:
-                        continue
-                elif (RSI[peak] <= 32):
-                    # Hidden divergence:
-                    if (HAdf_5mn.iloc[[peak]].Close[0] > HAdf_5mn.iloc[[currect_pic]].Close[0] and \
-                            RSI[peak] < RSI[currect_pic]):
-                        Div = True
-                        print("Regular Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
-                        break
-                    else:
-                        continue
+        Div = divergence_verificaiton(uncrossed_peaks_RSI,OB_or_OS,RSI,HAdf_5mn,currect_pic)
+
 
         # 5 --- buy or sell
         if(Div):
-            print("Must buy or sell")
+            print("Div = True")
+            logging.info("Div = True")
             # Get usdt futurse balance
             try:
                 balance = pd.DataFrame(client.futures_account_balance())
@@ -244,29 +155,37 @@ def div_5min(symbol = "WAVESUSDT", window_div= 7, tolerance = 0.25, levier = 1):
                                                        type='STOP_MARKET', quantity=qty, stopPrice=round(SL, 3))
             except Exception as e:
                 print(f'Problem in firing the order, exception hya : {e}')
+                logging.info(f'Problem in firing the order, exception hya : {e}')
 
             try:
                 a = pd.DataFrame(client.futures_position_information())
                 a = a.loc[pd.to_numeric(a.entryPrice) > 0,]
             except Exception as e:
                 print(f'Problem in futures_position_information, exception hya : {e}')
+                logging.info((f'Problem in futures_position_information, exception hya : {e}'))
+
             while(len(a.index)>0):
                 try:
-                    print("Still in position")
+                    print("Still in position, time = " + str(datetime.now()))
+                    logging.info("Still in position, time = " + str(datetime.now()))
                     time.sleep(15)
-                    print("time = " + str(datetime.now()))
                     a = pd.DataFrame(client.futures_position_information())
                     a = a.loc[pd.to_numeric(a.entryPrice) > 0,]
                 except Exception as e:
                     print(f'Problem in futures_position_information (inside the loop), exception hya : {e}')
+                    logging.info(f'Problem in futures_position_information (inside the loop), exception hya : {e}')
+
             print("order (maybe) completed")
+            logging.info("order (maybe) completed")
             # Cancel all open orders
             try:
                 client.futures_cancel_all_open_orders(symbol= symbol)
             except Exception as e:
                 print(f'Problem in futures_cancel_all_open_orders, exception hya : {e}')
+                logging.info(f'Problem in futures_cancel_all_open_orders, exception hya : {e}')
         else:
             print("skip (no divergence detected)")
+            logging.info("skip (no divergence detected)")
             continue
 
 
@@ -305,3 +224,8 @@ div_5min(symbol= "WAVESUSDT")
 #     print(f'NameError hya: {n}')
 # except Exception as e:
 #        print(f'Exception hya: {e}')
+
+
+
+
+import logging
--- functions/math_tools.py
+++ functions/math_tools.py
@@ -1,8 +1,8 @@
 
 # Author: Ahmed / Yasine
 # 26/04/2022
-# Function that eliminates crossed pics
 
+# --- 1 keep uncrossed preaks rpice
 def keep_uncrossed_peaks(close,open,peaks,tolerance,Short):
 
     uncrossed_peaks = []
@@ -38,7 +38,7 @@ def keep_uncrossed_peaks(close,open,peaks,tolerance,Short):
     return(uncrossed_peaks)
 
 
-# Uncrossed peaks for RSI
+# --- 2 keep uncrossed peaks rsi
 def keep_uncrossed_peaks_RSI(RSI,peaks,Short):
 
     uncrossed_peaks = []
@@ -66,5 +66,165 @@ def keep_uncrossed_peaks_RSI(RSI,peaks,Short):
             ):
                 uncrossed_peaks.append(peak)
     return(uncrossed_peaks)
+
+# --- 3 Transformation from normal candles to HA
+def HA_transformation(DF):
+    HAdf = DF[['Open', 'High', 'Low', 'Close']]
+    HAdf['Close'] = (DF['Open'] + DF['Close'] + DF['Low'] + DF['High']) / 4 # ignore error, just warning
+    for i in range(len(DF)):
+        if i == 0:
+            HAdf.iat[0, 0] = round(((DF['Open'].iloc[0] + DF['Close'].iloc[0]) / 2), 2)
+        else:
+            HAdf.iat[i, 0] = round(((HAdf.iat[i - 1, 0] + HAdf.iat[i - 1, 3]) / 2), 2)
+    HAdf['High'] = HAdf.loc[:, ['Open', 'Close']].join(DF['High']).max(axis=1)
+    HAdf['Low'] = HAdf.loc[:, ['Open', 'Close']].join(DF['Low']).min(axis=1)
+    return(HAdf)
+
+# --- 4 Verification stochastic 5min
+def verify_stochastic_5min(OB_or_OS, RSI_stoch_d, RSI_stoch_k):
+    if (OB_or_OS == 2):
+        # last two candles are green
+        if (((RSI_stoch_d.iloc[[-1]] >= 80) & (RSI_stoch_k.iloc[[-1]] >= 80))[0]):
+            print("next steps: short (stochastic is verified)")
+            logging.info("next steps: short (stochastic is verified)")
+            return(True)
+        else:
+            print("skip: step 2 (stochastic not verified)")
+            logging.info("skip: step 2 (stochastic not verified)")
+            return(False)
+    elif (OB_or_OS == 0):
+        # last two candles are red
+        if (((RSI_stoch_d.iloc[[-1]] <= 20) & (RSI_stoch_k.iloc[[-1]] <= 20))[0]):
+            print("next steps: long (stochastic is verified)")
+            logging.info("next steps: long (stochastic is verified)")
+            return(True)
+        else:
+            print("skip: step 2 (stochastic not verified)")
+            logging.info("skip: step 2 (stochastic not verified)")
+            return(False)
+    else:
+        print("skip: last two candles are not in the same color!")
+        logging.info("skip: last two candles are not in the same color!")
+        return(False)
+
+# --- 5 Verification condition 5 min
+def verify_condition_5min(OB_or_OS,HAdf_5mn):
+    # 3 - a - position short
+    if (OB_or_OS == 2):
+        # {High(t) - Low(t)} should be lower than {High(t-1) - Low(t-1)}
+        if ((HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[1] <= (HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[0]):
+            print("next steps (Short): Verify div (condition 5 min verified)")
+            logging.info("next steps (Short): Verify div (condition 5 min verified)")
+            return(True)
+        else:
+            print("skip (condition 5 min NOT verified)")
+            logging.info("skip (condition 5 min NOT verified)")
+            return(False)
+    # 3 - b - position Long
+    if (OB_or_OS == 0):
+        # {High(t) - Low(t)} should be lower than {High(t-1) - Low(t-1)}
+        if ((HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[1] <= (HAdf_5mn.tail(2).High - HAdf_5mn.tail(2).Low)[0]):
+            print("next steps (Long): Verify div (condition 5 min verified)")
+            logging.info("next steps (Long): Verify div (condition 5 min verified)")
+            return(True)
+        else:
+            print("skip (condition 5 min NOT verified)")
+            logging.info("skip (condition 5 min NOT verified)")
+            return(False)
+
+# --- 6 Function to eliminate invalid last peaks (Stochastic + 3 candles verification)
+def eliminate_last_pics_stocha_candles(OB_or_OS,peaks,HAdf_5mn,RSI_stoch_k):
+    if (OB_or_OS == 2):
+        # short
+        potential_pics = []
+        for idx in peaks:
+            # Stochastic must be verified
+            if (((RSI_stoch_k.iloc[[idx]] >= 65))[0]):
+                # At least 3 candles same color
+                if (
+                        sum(HAdf_5mn.iloc[[idx - 1, idx, idx + 1]].Close > HAdf_5mn.iloc[
+                            [idx - 1, idx, idx + 1]].Open) == 3 or \
+                        sum(HAdf_5mn.iloc[[idx - 2, idx - 1, idx]].Close > HAdf_5mn.iloc[
+                            [idx - 2, idx - 1, idx]].Open) == 3 or \
+                        sum(HAdf_5mn.iloc[[idx, idx + 1, idx + 2]].Close > HAdf_5mn.iloc[
+                            [idx, idx + 1, idx + 2]].Open) == 3
+                ):
+                    potential_pics.append(idx)
+                else:
+                    continue
+            else:
+                continue
+        return(potential_pics)
+    else:
+        # long
+        potential_pics = []
+        for idx in peaks:
+            # Stochastic must be verified
+            if (((RSI_stoch_k.iloc[[idx]] <= 35))[0]):
+                # At least 3 candles
+                if (
+                        sum(HAdf_5mn.iloc[[idx - 1, idx, idx + 1]].Close < HAdf_5mn.iloc[
+                            [idx - 1, idx, idx + 1]].Open) == 3 or \
+                        sum(HAdf_5mn.iloc[[idx - 2, idx - 1, idx]].Close < HAdf_5mn.iloc[
+                            [idx - 2, idx - 1, idx]].Open) == 3 or \
+                        sum(HAdf_5mn.iloc[[idx, idx + 1, idx + 2]].Close < HAdf_5mn.iloc[
+                            [idx, idx + 1, idx + 2]].Open) == 3
+                ):
+                    potential_pics.append(idx)
+                else:
+                    continue
+            else:
+                continue
+        return (potential_pics)
+
+# --- 7 Function to eliminate peaks without divergence (must be reviewed)
+def divergence_verificaiton(uncrossed_peaks_RSI,OB_or_OS,RSI,HAdf_5mn,currect_pic):
+    Div = False
+    for peak in uncrossed_peaks_RSI:
+        if (OB_or_OS == 2):
+            # Short
+            # Hidden divergence:
+            if (32 < RSI[peak] < 68):
+                if (HAdf_5mn.iloc[[peak]].Close[0] > HAdf_5mn.iloc[[currect_pic]].Close[0] and \
+                        RSI[peak] < RSI[currect_pic]):
+                    Div = True
+                    print("Hidden Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    logging.info("Hidden Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    break
+                else:
+                    continue
+            elif (RSI[peak] >= 68):
+                # Regular divergence:
+                if (HAdf_5mn.iloc[[peak]].Close[0] < HAdf_5mn.iloc[[currect_pic]].Close[0] and \
+                        RSI[peak] > RSI[currect_pic]):
+                    Div = True
+                    print("Regular Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    logging.info("Regular Div detected for 'Short' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    break
+                else:
+                    continue
+        else:
+            # Long
+            # Hidden divergence:
+            if (32 < RSI[peak] < 68):
+                if (HAdf_5mn.iloc[[peak]].Close[0] < HAdf_5mn.iloc[[currect_pic]].Close[0] and \
+                        RSI[peak] > RSI[currect_pic]):
+                    Div = True
+                    print("Hidden Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    logging.info("Hidden Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    break
+                else:
+                    continue
+            elif (RSI[peak] <= 32):
+                # Regular divergence:
+                if (HAdf_5mn.iloc[[peak]].Close[0] > HAdf_5mn.iloc[[currect_pic]].Close[0] and \
+                        RSI[peak] < RSI[currect_pic]):
+                    Div = True
+                    print("Regular Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    logging.info("Regular Div detected for 'Long' in: " + str(HAdf_5mn.iloc[[peak]].index))
+                    break
+                else:
+                    continue
+    return(Div)
 
 print("'math_tools.py' has been Sucessfully executed ")
